6806c8fb793358afa222f0b50317a7f86a73dc86
Davey <davey@myiworlds.com>
2018-11-13 19:36:27 -0800

    Fixing env var problem

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,7 +3,9 @@ import * as request from 'request';
 
 export const meetupEventsApi = functions.https.onRequest(async (req, res) => {
   await request(
-    `https://api.meetup.com/GDGCloudVancouver/events?&key=MEETUP_API_KEY_GOES_HERE&sign=true&photo-host=public&page=20&fields=featured_photo`,
+    `https://api.meetup.com/GDGCloudVancouver/events?&key=${
+      functions.config().meetup.key
+    }&sign=true&photo-host=public&page=20&fields=featured_photo`,
     (error, response, body) => {
       if (!error && response.statusCode === 200) {
         res.set('Access-Control-Allow-Origin', '*');
